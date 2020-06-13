035be7f752844dd23bf673f38c87680f1498c211
truncet <theonlyeminem@gmail.com>
2020-06-13 17:23:13 +0545

    settings issue

--- Bangla/settings.py
+++ Bangla/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 PROJECT_ROOT  = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS= [os.path.join(PROJECT_ROOT,'static'),]
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'static'),]
 STATICFILES_STORATGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL="/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')

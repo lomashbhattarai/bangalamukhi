eb9d6756834f8695775ea36bc1984d80248498ab
truncet <theonlyeminem@gmail.com>
2020-06-13 17:20:16 +0545

    db issue fixed

--- Bangla/settings.py
+++ Bangla/settings.py
@@ -28,8 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['bangalamukhi.herokuapp.com']
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 
 # Application definition
 
@@ -85,6 +83,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
